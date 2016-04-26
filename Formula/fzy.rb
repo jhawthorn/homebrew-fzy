5be848204bf722dddddc8aae58cc629441bec7be
John Hawthorn <john.hawthorn@gmail.com>
2016-04-25 23:11:29 -0700

    Fix sha1

--- Formula/fzy.rb
+++ Formula/fzy.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Fzy < Formula
   homepage 'https://github.com/jhawthorn/fzy#readme'
   url 'https://github.com/jhawthorn/fzy/archive/0.3.tar.gz'
-  sha1 '2a0c15d51f68d6f5bd05e8129b651264'
+  sha1 '5f4c25eb8fadf4f177a2bb5dd6761e21bf2d205e'
 
   head 'https://github.com/jhawthorn/fzy.git'
 
