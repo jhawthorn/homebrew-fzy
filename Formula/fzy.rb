369bf0d7b529e2e2dd2950041d0b2ad154d20c19
John Hawthorn <john.hawthorn@gmail.com>
2016-07-26 19:02:06 -0700

    Version 0.6

--- Formula/fzy.rb
+++ Formula/fzy.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fzy < Formula
   homepage 'https://github.com/jhawthorn/fzy#readme'
-  url 'https://github.com/jhawthorn/fzy/archive/0.5.tar.gz'
-  sha1 'cba9775db95eebcc1de1c4a746b04a496b088567'
+  url 'https://github.com/jhawthorn/fzy/archive/0.6.tar.gz'
+  sha1 'f13e156c9416c70cc233c334df51b3c4e1691ebd'
 
   head 'https://github.com/jhawthorn/fzy.git'
 
